d80d194dd16ee1b381751745457b904b9d40a77c
raphael <raphaelcamachado@gmail.com>
2024-10-07 17:08:38 -0300

    Finishing API development

--- api/api/app.py
+++ api/api/app.py
@@ -1,16 +1,39 @@
-import owm.omw as owm
-from fastapi import FastAPI, HTTPException
-from github import Github
+import os
+from http import HTTPStatus
 
-owm_sdk = owm.OpenWeatherMapSDK("")
-github = Github("")
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from github import Github, InputFileContent
+from owm import owm
+
+from api.utils.message_formater import format_weather_data
+
+load_dotenv()
+
+owm_sdk = owm.OpenWeatherMapSDK(os.getenv('OWM_KEY'))
+github = Github(os.getenv('GITHUB_KEY'))
 app = FastAPI()
 
-@app.get("/weather",)
-async def get_weather(city: str):
+
+@app.get('/weather/{city}')
+async def get_weather(city: str) -> str:
     if not city:
-        raise HTTPException(status_code=400, detail="City parameter is required")
-    
-    weather_data = owm_sdk.get_weather("são José dos campos")
-    
-    return weather_data
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='City parameter is required',
+        )
+    weather_data = owm_sdk.get_weather(city)
+    if (
+        weather_data['status']
+        and weather_data['status'] == HTTPStatus.NOT_FOUND
+    ):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail=f'City {city} not found'
+        )
+    message = format_weather_data(weather_data['data'])
+    gist = github.get_user().create_gist(
+        public=False,
+        files={f'{city}_weather.md': InputFileContent(message)},
+    )
+
+    return gist.html_url
--- api/api/schemas/WeatherResponse.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class WeatherResponse(BaseModel):
-    weather: dict
-    gist_url: str
