84ca5116870e648376e896d70378850be5220247
raphael <raphaelcamachado@gmail.com>
2024-10-10 15:01:19 -0300

    Adding README.md and enum to translate the weather to portuguese

---
+++ api/api/enums/weather_translation_enum.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class WeatherTranslation(Enum):
+    CLEAR = "Céu Limpo"
+    CLOUDS = "Nublado"
+    RAIN = "Chuva"
+    DRIZZLE = "Chuvisco"
+    THUNDERSTORM = "Tempestade"
+    SNOW = "Neve"
+    MIST = "Névoa"
+    FOG = "Neblina"
+    HAZE = "Neblina Seca"
+    SMOKE = "Fumaça"
+    DUST = "Poeira"
+    SAND = "Areia"
+    ASH = "Cinzas"
+    SQUALL = "Rajada"
+    TORNADO = "Tornado"
+
+    @classmethod
+    def translate(cls, english_weather):
+        try:
+            return cls[english_weather.upper()].value
+        except KeyError:
+            return english_weather
